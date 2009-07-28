c9fb5de9f1666dd8a426e87127dc5aab3606c789
Tim Olsen <tolsen@limelabs.com>
2009-07-28 11:34:45 -0400

    Add git support to limestone/limestone/version
    Have LimebitsVersion.get_git_version_info() take in a directory path
    to chdir to before running git commands so that we can have it run in
    the superproject
    
    Have 'tag' default to null in get_git_version_info()

--- lib/limebits_version.rb
+++ lib/limebits_version.rb
@@ -2,33 +2,35 @@
 module LimebitsVersion
 
   class << self
-    def get_git_version_info
+    def get_git_version_info dir_path = '.'
 
-      git_branch_string = `git branch --no-color | grep \\* | cut -d" " -f2`.strip
-      git_commit_string = `git log -n1 --format=%H`.strip
+      Dir.chdir dir_path do 
 
-      git_tag_string = ''
-      for tag in `git for-each-ref --format='%(*objectname) %(refname:short)' --sort=-taggerdate refs/tags`
-        if tag.include? git_commit_string
-          git_tag_string = tag.split(' ', 2).last.strip
-          break
+        git_branch_string = `git branch --no-color | grep \\* | cut -d" " -f2`.strip
+        git_commit_string = `git log -n1 --format=%H`.strip
+
+        git_tag_string = nil
+        for tag in `git for-each-ref --format='%(*objectname) %(refname:short)' --sort=-taggerdate refs/tags`
+          if tag.include? git_commit_string
+            git_tag_string = tag.split(' ', 2).last.strip
+            break
+          end
         end
+
+        git_status_string = `git status`
+        git_status_a_exit_code = system 'git status -a > /dev/null'
+        datetime_string = Time.new.inspect
+
+        return {
+          'tag'         => git_tag_string,
+          'branch'      => git_branch_string,
+          'commit'      => git_commit_string,
+          'status'      => git_status_string,
+          'pristine'    => (not git_status_a_exit_code),
+          'deployed_at' => datetime_string
+        }
       end
-
-      git_status_string = `git status`
-      git_status_a_exit_code = system 'git status -a > /dev/null'
-      datetime_string = Time.new.inspect
-
-      return {
-        'tag'         => git_tag_string,
-        'branch'      => git_branch_string,
-        'commit'      => git_commit_string,
-        'status'      => git_status_string,
-        'pristine'    => (not git_status_a_exit_code),
-        'deployed_at' => datetime_string
-      }
     end
-    
   end
   
 end
