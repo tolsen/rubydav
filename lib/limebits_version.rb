e9901a1f25b947f058f9581678a8e0f6d094c98e
Tim Olsen <tolsen@limelabs.com>
2009-07-28 14:04:01 -0400

    exit carefully from Dir.chdir do block so that it cd's out of the directory
    add LimebitsVersion::execute() and use it to wrap shell calls in
    LimebitsVersion::get_git_version_info()

--- lib/limebits_version.rb
+++ lib/limebits_version.rb
@@ -3,24 +3,28 @@ module LimebitsVersion
 
   class << self
     def get_git_version_info dir_path = '.'
+      return Dir.chdir(dir_path) do 
 
-      Dir.chdir dir_path do 
-
-        git_branch_string = `git branch --no-color | grep \\* | cut -d" " -f2`.strip
-        git_commit_string = `git log -n1 --format=%H`.strip
+        git_branch_string =
+          execute('set -o pipefail; git branch --no-color | grep \\* | cut -d" " -f2').strip
+        git_commit_string = execute('git log -n1 --format=%H').strip
 
         git_tag_string = nil
-        for tag in `git for-each-ref --format='%(*objectname) %(refname:short)' --sort=-taggerdate refs/tags`
+        for tag in execute("git for-each-ref --format='%(*objectname)" +
+                           " %(refname:short)' --sort=-taggerdate refs/tags")
           if tag.include? git_commit_string
             git_tag_string = tag.split(' ', 2).last.strip
             break
           end
         end
 
-        git_status_string = `git status`
-        git_status_a_exit_code = system 'git status -a > /dev/null'
+        git_status_string = execute("git status", [0, 1])
 
-        return {
+        git_status_a_exit_code = system 'git status -a > /dev/null'
+        raise "git status -a failed: #{$?}" unless
+          [0, 1].include? $?.exitstatus
+
+        next {
           'tag'         => git_tag_string,
           'branch'      => git_branch_string,
           'commit'      => git_commit_string,
@@ -29,6 +33,16 @@ module LimebitsVersion
         }
       end
     end
+
+    private
+
+    def execute(cmd, successful_exit_codes = [0])
+      out = `#{cmd}`
+      raise "Failed with exit code #{$?}: #{cmd}" unless
+        successful_exit_codes.include? $?.exitstatus
+      return out
+    end
+    
   end
   
 end
