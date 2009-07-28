d510d0ca38a85f6b2476e6c157a498492d94d782
Tim Olsen <tolsen@limelabs.com>
2009-07-28 14:24:37 -0400

    Fix printing out of exitcode to print the proper exitcode
    Have LimebitsVersion::execute() return an array of information

--- lib/limebits_version.rb
+++ lib/limebits_version.rb
@@ -6,30 +6,27 @@ module LimebitsVersion
       return Dir.chdir(dir_path) do 
 
         git_branch_string =
-          execute('set -o pipefail; git branch --no-color | grep \\* | cut -d" " -f2').strip
-        git_commit_string = execute('git log -n1 --format=%H').strip
+          execute('set -o pipefail; git branch --no-color | grep \\* | cut -d" " -f2')[0].strip
+        git_commit_string = execute('git log -n1 --format=%H')[0].strip
 
         git_tag_string = nil
         for tag in execute("git for-each-ref --format='%(*objectname)" +
-                           " %(refname:short)' --sort=-taggerdate refs/tags")
+                           " %(refname:short)' --sort=-taggerdate refs/tags")[0]
           if tag.include? git_commit_string
             git_tag_string = tag.split(' ', 2).last.strip
             break
           end
         end
 
-        git_status_string = execute("git status", [0, 1])
-
-        git_status_a_exit_code = system 'git status -a > /dev/null'
-        raise "git status -a failed: #{$?}" unless
-          [0, 1].include? $?.exitstatus
+        git_status_string = execute("git status", [0, 1])[0]
+        git_status_a_exit_code = execute('git status -a > /dev/null', [0, 1])[1]
 
         next {
           'tag'         => git_tag_string,
           'branch'      => git_branch_string,
           'commit'      => git_commit_string,
           'status'      => git_status_string,
-          'pristine'    => (not git_status_a_exit_code),
+          'pristine'    => (git_status_a_exit_code == 1),
         }
       end
     end
@@ -38,9 +35,10 @@ module LimebitsVersion
 
     def execute(cmd, successful_exit_codes = [0])
       out = `#{cmd}`
-      raise "Failed with exit code #{$?}: #{cmd}" unless
-        successful_exit_codes.include? $?.exitstatus
-      return out
+      exitcode = $?.exitstatus
+      raise "Failed with exit code #{exitcode}: #{cmd}" unless
+        successful_exit_codes.include? exitcode
+      return [out, exitcode, $?]
     end
     
   end
