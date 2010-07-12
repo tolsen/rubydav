058f8d336e60a3d0fb159ed66f397bfd1d347bc8
Tim Olsen <tolsen@limelabs.com>
2010-07-12 14:24:25 -0400

    change DomainMap to act like a Hash

--- lib/limestone/domain_map.rb
+++ lib/limestone/domain_map.rb
@@ -3,88 +3,54 @@ require File.dirname(__FILE__) + '/../rubydav/property_result'
 
 module RubyDav
 
-  class DomainMap
+  class DomainMap < Hash
 
-    attr_reader :entries
-
-    def == other
-      other.is_a?(DomainMap) && entries == other.entries
-    end
-
-    alias :eql? :==
-
-    def initialize *entries
-      @entries = entries
+    def initialize hsh = {}
+      merge! hsh
     end
 
     def to_inner_xml
-      @entries.map { |e| e.to_xml }.join("\n")
+      map { |d, p| self.class.entry_to_xml d, p }.join("\n")
     end
 
     def to_xml xml = nil
       return RubyDav.build_xml(xml) do |xml, namespaces|
         namespaces['xmlns:lb'] = LIMEBITS_NS
         xml.lb :'domain-map', namespaces do
-          @entries.each { |e| e.to_xml xml }
+          each { |d, p| self.class.entry_to_xml d, p, xml }
         end
       end
     end
+    
 
     class << self
-      
+
+      def entry_to_xml domain, path, xml = nil
+        return RubyDav.build_xml(xml) do |xml, namespaces|
+          namespaces['xmlns:lb'] = LIMEBITS_NS
+          xml.lb :'domain-map-entry', namespaces do
+            xml.lb :domain, domain
+            xml.lb :path, path
+          end
+        end
+      end
+
       def from_elem elem
         RubyDav.assert_elem_name elem, 'domain-map', LIMEBITS_NS
-        entries = RubyDav.elements_named(elem, 'domain-map-entry',
-                                         LIMEBITS_NS).map do |e|
-          DomainMapEntry.from_elem e
+        dm = new
+        
+        RubyDav.elements_named(elem, 'domain-map-entry',
+                                     LIMEBITS_NS).each do |e|
+          domain_elem = RubyDav.first_element_named e, 'domain', LIMEBITS_NS
+          path_elem = RubyDav.first_element_named e, 'path', LIMEBITS_NS
+          dm[domain_elem.content] = path_elem.content
         end
         
-        return new(*entries)
+        return dm
       end
-      
+
     end
-
-    PropertyResult.define_class_reader(:domain_map, self,
-                                       'domain-map', LIMEBITS_NS)
-  end
-
-  
-  class DomainMapEntry
-
-    attr_reader :domain, :path
-
-    def == other
-      other.is_a?(DomainMapEntry) &&
-        domain == other.domain &&
-        path == other.path
-    end
-
-    alias :eql? :==
-
-    def initialize domain, path
-      @domain = domain
-      @path = path
-    end
-
-    def to_xml xml = nil
-      return RubyDav.build_xml(xml) do |xml, namespaces|
-        namespaces['xmlns:lb'] = LIMEBITS_NS
-        xml.lb :'domain-map-entry', namespaces do
-          xml.lb :domain, domain
-          xml.lb :path, path
-        end
-      end
-    end
-
-    class << self
-
-      def from_elem elem
-        domain_elem = RubyDav.first_element_named elem, 'domain', LIMEBITS_NS
-        path_elem = RubyDav.first_element_named elem, 'path', LIMEBITS_NS
-
-        return new(domain_elem.content, path_elem.content)
-      end
-    end
+    
   end
   
 end
--- test/unit/domain_map_test.rb
+++ test/unit/domain_map_test.rb
@@ -2,59 +2,6 @@ require 'test/unit/unit_test_helper'
 
 require 'lib/limestone'
 
-class DomainMapEntryTest < RubyDavUnitTestCase
-
-  def setup
-    super
-    @entry = RubyDav::DomainMapEntry.new 'www.example.com', '/home/tim/example'
-  end
-
-  def test_eqeq__equal
-    entry2 = RubyDav::DomainMapEntry.new 'www.example.com', '/home/tim/example'
-    assert_equal @entry, entry2
-  end
-
-  def test_eqeq__not_equal
-    entry2 = RubyDav::DomainMapEntry.new 'www.example2.com', '/home/tim/example'
-    entry3 = RubyDav::DomainMapEntry.new 'www.example.com', '/home/tim/example2'
-
-    assert_not_equal @entry, entry2
-    assert_not_equal @entry, entry3
-  end
-
-  def test_from_elem
-    domain_map_entry_str = <<EOS
-<lb:domain-map-entry xmlns:lb='http://limebits.com/ns/1.0/'>
-  <lb:domain>www.limelabs.com</lb:domain>
-  <lb:path>/home/limelabs</lb:path>
-</lb:domain-map-entry>
-EOS
-    domain_map_entry_elem =
-      LibXML::XML::Document.string(domain_map_entry_str).root
-
-    entry = RubyDav::DomainMapEntry.from_elem domain_map_entry_elem
-    
-    assert_equal 'www.limelabs.com', entry.domain
-    assert_equal '/home/limelabs', entry.path
-  end
-  
-  def test_initialize
-    assert_equal 'www.example.com', @entry.domain
-    assert_equal '/home/tim/example', @entry.path
-  end
-
-  def test_to_xml
-    assert_xml_matches @entry.to_xml do |xml|
-      xml.xmlns! :lb => 'http://limebits.com/ns/1.0/'
-      xml.lb :'domain-map-entry' do
-        xml.lb :domain, 'www.example.com'
-        xml.lb :path, '/home/tim/example'
-      end
-    end
-  end
-
-end
-
 class DomainMapTest < RubyDavUnitTestCase
 
   def assert_domain_map_matches xml
@@ -72,10 +19,8 @@ class DomainMapTest < RubyDavUnitTestCase
   end
 
   def setup
-    @entry1 = RubyDav::DomainMapEntry.new 'domain1', 'path1'
-    @entry2 = RubyDav::DomainMapEntry.new 'domain2', 'path2'
-
-    @domain_map = RubyDav::DomainMap.new @entry1, @entry2
+    @domain_map = RubyDav::DomainMap.new('domain1' => 'path1',
+                                         'domain2' => 'path2')
   end
 
   def test_from_elem
@@ -96,11 +41,12 @@ EOS
 
     domain_map = RubyDav::DomainMap.from_elem domain_map_elem
 
-    assert_equal [@entry1, @entry2], domain_map.entries
+    assert_equal @domain_map, domain_map
   end
 
   def test_initialize
-    assert_equal [@entry1, @entry2], @domain_map.entries
+    assert_equal([['domain1', 'path1'], ['domain2', 'path2']],
+                 @domain_map.to_a.sort)
   end
 
   def test_to_xml
